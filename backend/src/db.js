0e139e3b4034a36b89ea9b3a81ce9149d42e4d24
Deeptikushwaha <deeptikushwaha140@gmail.com>
2025-03-27 12:17:20 +0530

    fixed:mongoose error

--- backend/src/db.js
+++ backend/src/db.js
@@ -4,7 +4,11 @@ const config = require('./config');
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL)
+console.log("MONGO_URL:", process.env.MONGO_URL);
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 const userSchema = new mongoose.Schema({
     username: {
